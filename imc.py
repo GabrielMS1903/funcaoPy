7c78ab09e82d0c9760e4630f5ed72b20c6df03f1
GabrielMS1903 <149096770+GabrielMS1903@users.noreply.github.com>
2023-12-17 19:28:40 -0300

    Update imc.py

--- imc.py
+++ imc.py
@@ -4,3 +4,17 @@ def joao(peso, altura):
 
 numero = joao(56, 1.70)
 print(numero)
+
+
+def op(num1, num2, operacao):
+ if operacao == 2:
+  return num1 - num2
+ elif operacao == 1:
+   return num1 + num2
+ elif num2 > num1:
+  return num1 * num2
+ elif num1 > num2: 
+  return num1 / num2
+
+resultado = op(10, 2, 1)
+print(resultado)
